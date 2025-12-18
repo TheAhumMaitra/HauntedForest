2baf204aef73982dbb1f821b47215346aa5dcf81
Ahum Maitra <theahummaitra@gmail.com>
2025-12-18 19:23:38 +0530

    Enhanced the `Hero Section`, mobile first then larger screen

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -5,7 +5,7 @@ export default function HeroSection() {
         <h1 className="font-bold underline decoration-purple-700 text-transparent bg-clip-text bg-linear-to-r from-purple-500 to-pink-400 text-4xl text-center">
           Hello World! I am Ahum
         </h1>
-        <p className="lg:text-2xl sm:text-xl font-bold sm:w-xl text-transparent bg-clip-text bg-linear-to-r from-cyan-500 to-green-400 text-wrap">
+        <p className="text-xl sm:text-2xl font-bold sm:w-4xl text-transparent bg-clip-text bg-linear-to-r from-cyan-500 to-green-400 text-wrap">
           Welcome to my beautiful website. I am a programmer, who loves programming! I love Python, C, Rust, JS, TS, frontend and backend works!
         </p>
       </div>

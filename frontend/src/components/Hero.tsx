402064a2666b776a3ddeb6580d3ab919241baeb6
Ahum Maitra <theahummaitra@gmail.com>
2025-12-18 17:29:36 +0530

    Fixed text size issue in mobile

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -5,7 +5,7 @@ export default function HeroSection() {
         <h1 className="font-bold underline decoration-purple-700 text-transparent bg-clip-text bg-linear-to-r from-purple-500 to-pink-400 text-4xl text-center">
           Hello World! I am Ahum
         </h1>
-        <p className="lg:text-2xl sm:text-xl overflow-clip font-bold w-2xl text-transparent bg-clip-text bg-linear-to-r from-cyan-500 to-green-400 text-wrap">
+        <p className="lg:text-2xl sm:text-xl font-bold sm:w-xl text-transparent bg-clip-text bg-linear-to-r from-cyan-500 to-green-400 text-wrap">
           Welcome to my beautiful website. I created this website using Next JS
           16, ReactBits. It is open source on Github. Feel free to know about me
           and contact me!

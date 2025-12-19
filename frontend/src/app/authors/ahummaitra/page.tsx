97f24122359f21b164d99e3a92dd901db2911fa3
Ahum Maitra <theahummaitra@gmail.com>
2025-12-19 16:36:10 +0530

    Add `LiquidEther` effect in `authors/ahummaitra` page

--- frontend/src/app/authors/ahummaitra/page.tsx
+++ frontend/src/app/authors/ahummaitra/page.tsx
@@ -1,3 +1,4 @@
+import LiquidEther from "@/components/LiquidEther";
 import Shuffle from "@/components/Shuffle";
 import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
@@ -7,6 +8,26 @@ import { SiCodeberg } from "react-icons/si";
 export default function Page() {
   return (
     <>
+      <div className="absolute inset-0 -z-10 w-full h-200">
+        <LiquidEther
+          colors={["#5227FF", "#FF9FFC", "#B19EEF"]}
+          mouseForce={20}
+          cursorSize={100}
+          isViscous={false}
+          viscous={30}
+          iterationsViscous={32}
+          iterationsPoisson={32}
+          resolution={0.5}
+          isBounce={false}
+          autoDemo={true}
+          autoSpeed={0.5}
+          autoIntensity={2.2}
+          takeoverDuration={0.25}
+          autoResumeDelay={3000}
+          autoRampDuration={0.6}
+        />
+      </div>
+
       <div className="w-full min-h-screen p-3 flex flex-col justify-center items-center gap-10">
         <h1 className="text-center sm:text-5xl italic font-bold text-3xl text-purple-500">
           Hello, I am
@@ -26,7 +47,10 @@ export default function Page() {
             respectReducedMotion={true}
           />
         </div>
-        <p className="font-semibold italic border-b-cyan-400 p-3 rounded-2xl border-4">Hello, World! I'm a programmer, who loves Python, JS, TS, Web development Frontend, Backend, Go, Rust!</p>
+        <p className="font-semibold italic border-b-cyan-400 p-3 rounded-2xl border-4">
+          Hello, World! I'm a programmer, who loves Python, JS, TS, Web
+          development Frontend, Backend, Go, Rust!
+        </p>
         <div className="flex flex-col justify-between items-center gap-4 mt-2">
           <h2 className="font-bold text-xl">Badges: </h2>
           <span>

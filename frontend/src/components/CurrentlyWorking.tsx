73a22fb3d20d36c6f72e20c68c99b60097bcc41f
Ahum Maitra <theahummaitra@gmail.com>
2025-12-19 13:03:38 +0530

    Remove image preview for currently working projects

--- frontend/src/components/CurrentlyWorking.tsx
+++ frontend/src/components/CurrentlyWorking.tsx
@@ -6,13 +6,7 @@ export default function CurrentlyWprking() {
       <h2 className="xl sm:text-2xl mt-7 text-center font-bold">I am currently working on..</h2>
       <div className="w-full h-80 p-2 rounded-2xl">
         <Link href="https://github.com/TheAhumMaitra/Itomori">
-          <Image
-            alt="Itomori"
-            width={1000}
-            height={800}
-            className="w-full h-full scale-50 sm:scale-150"
-            src="https://gh-card.dev/repos/TheAhumMaitra/Itomori.svg"
-          />
+         <h2>Itomori</h2>
         </Link>
       </div>
     </div>

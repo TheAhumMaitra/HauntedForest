6e2ba9d0f35f9e69d68b2df0d85a4394c8e39915
Ahum Maitra <theahummaitra@gmail.com>
2026-02-06 21:10:16 +0530

    feat: Add FAQ'S about me

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,4 +1,5 @@
 import FuzzyText from "../../components/FuzzyText";
+import FAQ from "./faq";
 
 export default function Page() {
   return (
@@ -13,13 +14,11 @@ export default function Page() {
       </div>
       <div className="text-center mt-10 w-85 h-full sm:w-full text-wrap border-background shadow-2xl shadow-primary bg-card p-3 rounded-2xl font-bold border-4">
         <p className="w-full">
-          "Hello, World! I am Ahum. I am a programmer. I want to become an AI
-          and ML developer. I love programming. I am currently learning so many
-          stuffs. I use my primary language as Python. I love Python. I know
-          Python, Javascript, Go, Rust, Typescript, Kotlin. I know web
-          developing (frontend) and learning backend. I love Next JS. I am
-          learning video editing as a hobby. Thanks for visiting my website."
+          "Hello, World! I am Ahum. I am a Programmer and Computer Science Student. I have a good experience with Python and frontend web developing and software engineering. I usually create TUI'S as my hobby project. I'm doing CS50X. If you are more interested about me view the FAQ'S (down here) or visit my Github.
         </p>
+      </div>
+      <div className="w-full h-full flex flex-col justify-center items-center sm:m-8">
+      <FAQ />
       </div>
     </div>
   );
